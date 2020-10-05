f324bfeba9488640d14ad44f573fb571d3dd5639
Vitor <1697059+VitorRocha99@users.noreply.github.com>
2020-10-05 18:27:03 -0300

    Projeto com react youtube instalado

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import Constants from 'expo-constants';
-
 
 //importações do React Navigation
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 import { useRoute } from '@react-navigation/native';
-
 
 //módulo do Tab Navigator
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -24,318 +29,306 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-
-//header da home 
+//header da home
 // CABEÇALHO <<<<<<<
 function LogoTitle() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-            <Text style={styles.titulo}>Dorgan</Text>
-            <Image
-                style={{ width: 20, height: 35, marginTop: 5, marginLeft: 2 }}
-                source={require('./images/heartAlpha.jpg')}
-            />
+  return (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      <Text style={styles.titulo}>Dorgan</Text>
+      <Image
+        style={{ width: 20, height: 35, marginTop: 5, marginLeft: 2 }}
+        source={require('./images/heartAlpha.jpg')}
+      />
 
-            <Ionicons type="button" name="md-settings"
-                style={styles.button}
-
-                title='Configurações'
-                onPress={() => navigation.navigate('config')}
-
-            />
-
-
-        </View>
-    );
+      <Ionicons
+        type="button"
+        name="md-settings"
+        style={styles.button}
+        title="Configurações"
+        onPress={() => navigation.navigate('config')}
+      />
+    </View>
+  );
 }
 
 // TELA DE CONFIGURAÇÃO  <<<<<<<<
 function ConfigScreen() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="config"
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ color: '#3268a8', fontSize: 25, fontWeight: 'bold' }}>Configurações</Text>
-                        <TouchableOpacity
-                            onPress={() => navigation.navigate('sobre')}
-                            style={{ marginTop: 20 }}>
-                            <Text style={{ fontSize: 23, color: '#3268a8', marginTop: 50 }}>Sobre Dorgan</Text>
-                        </TouchableOpacity>
-                        <Text style={{ fontSize: 23, color: '#3268a8', marginTop: 50 }}>Notificações</Text>
-
-                        <Text style={{ fontSize: 23, color: '#3268a8', marginTop: 50 }}>Fale Conosco</Text>
-                        <Text style={{ fontSize: 20, color: '#3268a8', marginTop: 20 }}>Email: Dorgan@gmail.com</Text>
-                        <Text style={{ fontSize: 20, color: '#3268a8', marginTop: 20 }}>Telefone: 11 5577-8888</Text>
-
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="config"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text
+              style={{ color: '#3268a8', fontSize: 25, fontWeight: 'bold' }}>
+              Configurações
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('sobre')}
+              style={{ marginTop: 20 }}>
+              <Text style={{ fontSize: 23, color: '#3268a8', marginTop: 50 }}>
+                Sobre Dorgan
+              </Text>
+            </TouchableOpacity>
+            <Text style={{ fontSize: 23, color: '#3268a8', marginTop: 50 }}>
+              Fale Conosco
+            </Text>
+            <Text style={{ fontSize: 20, color: '#3268a8', marginTop: 20 }}>
+              Email: Dorgan@gmail.com
+            </Text>
+            <Text style={{ fontSize: 20, color: '#3268a8', marginTop: 20 }}>
+              Telefone: 11 5577-8888
+            </Text>
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
 //TELA DE SOBRE DORGAN
 function SobreScreen() {
-
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='sobre'
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ color: '#3268a8', fontSize: 25, fontWeight: 'bold' }}>Sobre Nós</Text>
-                        <Text style={{ fontSize: 25, color: '#fff' }}>Sobre Dorgan</Text>
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="sobre"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text
+              style={{ color: '#3268a8', fontSize: 25, fontWeight: 'bold' }}>
+              Sobre Nós
+            </Text>
+            <Text style={{ fontSize: 25, color: '#fff' }}>Sobre Dorgan</Text>
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
 // TELA DE DEPOIMENTO <<<<<<<<<
 function DepoimentosScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Depoimentos"
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text>Em processo :o </Text>
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Depoimentos"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Em processo :o </Text>
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
 // TELA DE JUNTE-SE A CAUSA <<<<<<<<<
 function JunteSeScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Junte-se a causa"
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ color: '#3268a8', fontSize: 30 }}>
-                            Junte-se a causa!
-                        </Text>
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Junte-se a causa"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: '#3268a8', fontSize: 30 }}>
+              Junte-se a causa!
+            </Text>
 
-                        <Image
-                            style={{ width: 100, height: 100, marginTop: 20 }}
-                            source={require('./images/facebook.png')}
-                        />
-                        <Text style={{ color: '#3268a8', fontSize: 15 }}>Facebook</Text>
-                        <Image
-                            style={{ width: 100, height: 100, marginTop: 20 }}
-                            source={require('./images/instagram.png')}
-                        />
-                        <Text style={{ color: '#3268a8', fontSize: 15 }}>Instagram</Text>
-                        <Image
-                            style={{ width: 100, height: 100, marginTop: 20 }}
-                            source={require('./images/whatsapp.png')}
-                        />
-                        <Text style={{ color: '#3268a8', fontSize: 15 }}>Whatsapp</Text>
-
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+            <Image
+              style={{ width: 100, height: 100, marginTop: 20 }}
+              source={require('./images/facebook.png')}
+            />
+            <Text style={{ color: '#3268a8', fontSize: 15 }}>Facebook</Text>
+            <Image
+              style={{ width: 100, height: 100, marginTop: 20 }}
+              source={require('./images/instagram.png')}
+            />
+            <Text style={{ color: '#3268a8', fontSize: 15 }}>Instagram</Text>
+            <Image
+              style={{ width: 100, height: 100, marginTop: 20 }}
+              source={require('./images/whatsapp.png')}
+            />
+            <Text style={{ color: '#3268a8', fontSize: 15 }}>Whatsapp</Text>
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
 // TELA DE DUVIDAS <<<<<<<<<
 function DuvidasScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="duvidas"
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text>Em Processo :)</Text>
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="duvidas"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Em Processo :)</Text>
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 // TELA INICIAL COM O CHAT <<<<<<<<<
 function InicialScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="inicial"
-                options={
-                    { headerTitle: props => <LogoTitle /> }}>
-                {props =>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text>Em Processo :)</Text>
-                        <Image
-                            style={{ width: 250, height: 350 }}
-                            source={require('./images/chat.png')}
-                        />
-                    </View>
-                }
-            </Stack.Screen>
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="inicial"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => (
+          <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Em Processo :)</Text>
+            <Image
+              style={{ width: 250, height: 350 }}
+              source={require('./images/chat.png')}
+            />
+          </View>
+        )}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
-
 function PrincipalStack() {
-    return (
-        <Stack.Navigator>
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        options={{ headerTitle: (props) => <LogoTitle /> }}>
+        {(props) => <InicialScreen />}
+      </Stack.Screen>
 
-            <Stack.Screen
-                name='Home'
-                options={{ headerTitle: props => <LogoTitle /> }}>
-                {props => <InicialScreen />}
-            </Stack.Screen>
+      <Stack.Screen name="config" options={{ title: 'Tela de configuração' }}>
+        {(props) => <ConfigScreen />}
+      </Stack.Screen>
 
-            <Stack.Screen
-                name='config'
-                options={{ title: 'Tela de configuração' }}>
-                {props => <ConfigScreen />}
-            </Stack.Screen>
+      <Stack.Screen name="Depoi" options={{ title: 'Depoimentos' }}>
+        {(props) => <DepoimentosScreen />}
+      </Stack.Screen>
 
-            <Stack.Screen
-                name='Depoi'
-                options={{ title: 'Depoimentos' }}>
-                {props => <DepoimentosScreen />}
-            </Stack.Screen>
+      <Stack.Screen name="JunteSe" options={{ title: 'Junte-se a causa' }}>
+        {(props) => <JunteSeScreen />}
+      </Stack.Screen>
 
-            <Stack.Screen
-                name='JunteSe'
-                options={{ title: 'Junte-se a causa' }}>
-                {props => <JunteSeScreen />}
-            </Stack.Screen>
-
-            <Stack.Screen
-                name='Duvidas'
-                options={{ title: 'Chat Duvidas' }}>
-                {props => <DuvidasScreen />}
-            </Stack.Screen>
-
-        </Stack.Navigator>
-
-    );
+      <Stack.Screen name="Duvidas" options={{ title: 'Chat Duvidas' }}>
+        {(props) => <DuvidasScreen />}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
 }
 
 //renderiza os botões inferiores (MENU) <<<<<<<
 function AppBottonTab({ routeName }) {
-    return (
-        <Tab.Navigator
-            initialRouteName={routeName}
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
+  return (
+    <Tab.Navigator
+      initialRouteName={routeName}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-                    if (route.name === 'Depoimentos') {
-                        iconName = 'ios-videocam';
-                    }
-                    else if (route.name === 'config') {
-                        iconName = focused ? 'md-settings' : 'md-settings';
-                    }
-                    else if (route.name === 'Junte-se a causa') {
-                        iconName = focused ? 'ios-people' : 'ios-people';
-                    }
-                    else if (route.name === 'Dúvidas') {
-                        iconName = focused ? 'ios-help-circle-outline' : 'ios-help-circle-outline';
-                    }
-                    else if (route.name === 'Inicial') {
-                        iconName = focused ? 'ios-home' : 'ios-home';
-                    }
+          if (route.name === 'Depoimentos') {
+            iconName = 'ios-videocam';
+          } else if (route.name === 'config') {
+            iconName = focused ? 'md-settings' : 'md-settings';
+          } else if (route.name === 'Junte-se a causa') {
+            iconName = focused ? 'ios-people' : 'ios-people';
+          } else if (route.name === 'Dúvidas') {
+            iconName = focused
+              ? 'ios-help-circle-outline'
+              : 'ios-help-circle-outline';
+          } else if (route.name === 'Inicial') {
+            iconName = focused ? 'ios-home' : 'ios-home';
+          }
 
-                    // Qualquer componente pode ser usado
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-            })}
-            tabBarOptions={{
-                activeTintColor: 'black',
-                inactiveTintColor: 'blue',
-            }}
-        >
-            <Tab.Screen name="Inicial" component={PrincipalStack} />
-            <Tab.Screen name="Depoimentos" component={DepoimentosScreen} />
-            <Tab.Screen name="Junte-se a causa" component={JunteSeScreen} />
-            <Tab.Screen name="Dúvidas" component={DuvidasScreen} />
-
-        </Tab.Navigator>
-
-    );
+          // Qualquer componente pode ser usado
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'blue',
+      }}>
+      <Tab.Screen name="Inicial" component={PrincipalStack} />
+      <Tab.Screen name="Depoimentos" component={DepoimentosScreen} />
+      <Tab.Screen name="Junte-se a causa" component={JunteSeScreen} />
+      <Tab.Screen name="Dúvidas" component={DuvidasScreen} />
+    </Tab.Navigator>
+  );
 }
 
 class App extends Component {
-    render() {
-        return (
-            <NavigationContainer>
-                <Drawer.Navigator>
+  render() {
+    return (
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="inicial">
+            {(props) => <AppBottonTab routeName="inicial" />}
+          </Drawer.Screen>
 
-                    <Drawer.Screen name='inicial'>
-                        {props => <AppBottonTab routeName='inicial' />}
-                    </Drawer.Screen>
+          <Drawer.Screen name="config">
+            {(props) => <AppBottonTab routeName="config" />}
+          </Drawer.Screen>
 
-                    <Drawer.Screen name='config'>
-                        {props => <AppBottonTab routeName='config' />}
-                    </Drawer.Screen>
+          <Drawer.Screen name="Depoimentos">
+            {(props) => <AppBottonTab routeName="Depoimentos" />}
+          </Drawer.Screen>
 
-                    <Drawer.Screen name='Depoimentos'>
-                        {props => <AppBottonTab routeName='Depoimentos' />}
-                    </Drawer.Screen>
+          <Drawer.Screen name="Junte-se a causa">
+            {(props) => <AppBottonTab routeName="Junte-se a causa" />}
+          </Drawer.Screen>
 
-                    <Drawer.Screen name='Junte-se a causa'>
-                        {props => <AppBottonTab routeName='Junte-se a causa' />}
-                    </Drawer.Screen>
-
-                    <Drawer.Screen name='Dúvidas'>
-                        {props => <AppBottonTab routeName='Dúvidas' />}
-                    </Drawer.Screen>
-
-                </Drawer.Navigator>
-            </NavigationContainer>
-        );
-    }
-} export default App
+          <Drawer.Screen name="Dúvidas">
+            {(props) => <AppBottonTab routeName="Dúvidas" />}
+          </Drawer.Screen>
+        </Drawer.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+export default App;
 
 const styles = StyleSheet.create({
-    paragraph: {
+  /*paragraph: {
         margin: 24,
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
-    },
-    titulo: {
-        margin: 1,
-        marginTop: 6,
-        fontSize: 23,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: "blue",
-    },
-    button: {
-        color: 'black',
-        fontSize: 28,
-        width: 150,
-        marginTop: 6,
-        marginHorizontal: 150,
-        paddingHorizontal: 10,
-        textAlign: 'center',
-        alignSelf: 'center',
-        activeTintColor: 'black',
-    },
+    },*/
+  titulo: {
+    margin: 1,
+    marginTop: 6,
+    fontSize: 23,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'blue',
+  },
+  button: {
+    color: 'black',
+    fontSize: 28,
+    width: 150,
+    marginTop: 6,
+    marginHorizontal: 150,
+    paddingHorizontal: 10,
+    textAlign: 'center',
+    alignSelf: 'center',
+    activeTintColor: 'black',
+  },
 });
